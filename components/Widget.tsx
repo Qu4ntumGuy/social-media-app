5c2cd042d89b58500475eb79547ba12f3d21eb9d
Qu4ntumGuy <gauravbisht8000@gmail.com>
2024-07-17 13:45:45 +0530

    add project

---
+++ components/Widget.tsx
@@ -0,0 +1,14 @@
+function Widget() {
+  return (
+    <div className="ml-6 h-[790px]">
+      <iframe
+        src="https://www.linkedin.com/embed/feed/update/urn:li:share:6950491440263106560"
+        title="Embedded post"
+        className="w-fit 2xl:min-w-[400px] h-full"
+      ></iframe>
+      {/* <iframe src="https://www.linkedin.com/embed/feed/update/urn:li:share:6950491440263106560" height="472" width="504" frameborder="0" allowfullscreen="" title="Embedded post"></iframe> */}
+    </div>
+  );
+}
+
+export default Widget;
